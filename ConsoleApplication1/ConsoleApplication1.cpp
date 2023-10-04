4b2385b564e125dfe1b7fb8a4ed192348a6858bf
Gallanthy <SheremetevPvl@yandex.ru>
2023-10-04 15:40:07 +0300

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+class Person
+{
+public:
+    Person(std::string name, unsigned age) : name{ name }, age{ age } {}
+    std::string getName() const { return name; }
+    unsigned getAge() const { return age; }
+
+    void setName(std::string personName) { name = personName; }
+    void setAge(unsigned personAge) { age = personAge; }
+private:
+    std::string name;
+    unsigned age{};
+};
+std::ostream& operator << (std::ostream& os, const Person& person)
+{
+    return os << person.getName() << " " << person.getAge();
+}
+
+void print(std::vector<Person> const& input)
+{
+    std::copy(input.begin(),
+        input.end(),
+       std::ostream_iterator<Person>(std::cout, " "));
+}
+
+int main()
+{
+    Person bob{ "",0 };
+    std::cout << "Input name and age: ";
+    vector<Person> myGroup =
+    {
+        Person{"Tom", 23},
+        Person{"Bob", 25},
+        Person{"Alice", 22},
+        Person{"Kate", 31}
+    };
+    print(myGroup);
+    //std::cin >> bob;
+    //std::cout << "Name: " << bob.getName() << "\tAge: " << bob.getAge() << std::endl;
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ StringRedactation/StringRedactation.cpp
@@ -0,0 +1,104 @@
+﻿// StringRedactation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+void InsertFunc(std::string& Str)
+{
+    int Pos(0);
+    std::string CharsForIns("");
+    std::cout << "<pose of insertion> <string for insertion>\n";
+    std::cin >> Pos >> CharsForIns;
+    std::cout << Str.insert(Pos, CharsForIns) << "\n";
+}
+
+void EraseFunc(std::string& Str)
+{
+    int Pos(0), Num(0);
+    std::cout << "<pose of erasing> <number of chars>\n";
+    std::cin >> Pos >> Num;
+    std::cout << Str.erase(Pos, Num) << "\n";
+}
+
+void ReplaceFunc(std::string& Str)
+{
+    int Pos(0), Num(0); 
+    std::string InsertStr("");
+    std::cout << "<pose of replacing> <number of chars> <string>\n";
+    std::cin >> Pos >> Num >> InsertStr;
+    std::cout << Str.replace(Pos, Num, InsertStr) << "\n";
+}
+
+void FindFunc(std::string& Str)
+{
+    std::string StrForLook("");
+    int Index(0);
+    std::cout << "Write a string you looking for\n";
+    std::cin >> StrForLook;
+    if (Str.find(StrForLook) != -1)
+    {
+        std::cout << "We found your string. Pos= " << Str.find(StrForLook) << "\n";
+    }
+    else
+    {
+        std::cout << "There is no this string here\n";
+    }
+}
+
+int main()
+{
+    std::string Str("");
+    int ComandIndex;
+    std::cout << "Write a string\n";
+    std::cin >> Str;
+    while (Str != "") 
+    {
+        std::cout << "Choose an option\n" << "1)insert, " << "2)erase, " << "3)replace, " << "4)find, "<< "5)end working, " << "6)use new string" << "\n";
+        std::cin >> ComandIndex;
+        switch(ComandIndex)
+        {
+            case 1:
+            {
+                InsertFunc(Str);
+                break;
+            }
+            case 2:
+            {
+                EraseFunc(Str);
+                break;
+            }
+            case 3:
+            {
+                ReplaceFunc(Str);
+                break;
+            }
+            case 4:
+            {
+                FindFunc(Str);
+                break;
+            }
+            case 5:
+            {
+                Str = "";
+                break;
+            }
+            case 6:
+            {
+                std::cout << "Write new string" << "\n";
+                std::cin >> Str;
+            }
+        }
+    }
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
