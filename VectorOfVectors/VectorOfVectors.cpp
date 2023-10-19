30287744a88e6026db33bfcc7fd846cfee4646c6
Gallanthy <SheremetevPvl@yandex.ru>
2023-10-19 22:08:44 +0300

    Create VectorOfVectors.cpp

---
+++ VectorOfVectors/VectorOfVectors.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    
+    std::vector<std::vector<int>> mainVector = { {1, 2, 3}, 
+                                                 {4, 5, 6}, 
+                                                 {7, 8, 9} };
+    std::vector<int> Vec;
+    for (int i = 0; i < mainVector.size(); i++)   //появляется непонятное значение 
+    {
+        Vec = mainVector.at(i);
+        for (int i = 0; i < Vec.size(); i++)
+        {
+            std::cout << Vec[i] << " ";
+        }
+        std::cout << '/n';
+    }
+    /*for (const auto& vector : mainVector)       //всё правильно выводится
+    {  
+        for (const auto& element : vector) 
+        {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+    */
+    return 1;
+}
