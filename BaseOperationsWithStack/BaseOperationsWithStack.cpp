d2fa80f3786062ea229fb027dc6a8afc6fa397a2
Gallanthy <SheremetevPvl@yandex.ru>
2023-10-19 22:46:54 +0300

    Create BaseOperationsWithStack.cpp

---
+++ BaseOperationsWithStack/BaseOperationsWithStack.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stack>
+
+const std::string askForStackSize = "How many elements do you need to add?";
+const std::string askForStackElements = "Enter this elements";
+const std::string sentFirstStackElShowing = "Top element is ";
+const std::string suggToDelTopStcakEl = "Now let`s delete first element";
+
+int main()
+{
+    int stackSize(0), stackElement;
+    std::stack<int> stackOfInt;
+    std::cout << askForStackSize << '\n';
+    std::cin >> stackSize;
+    std::cout << askForStackElements << '\n';
+    for (int i = 0; i < stackSize; i++)
+    {
+        std::cin >> stackElement;
+        stackOfInt.push(stackElement);
+    }
+    std::cout << sentFirstStackElShowing  << stackOfInt.top() << '\n';
+    std::cout << suggToDelTopStcakEl << '\n';
+    stackOfInt.pop();
+    std::cout << sentFirstStackElShowing << stackOfInt.top() << '\n';
+}
